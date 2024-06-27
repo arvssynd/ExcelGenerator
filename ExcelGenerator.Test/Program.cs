08fbff0f9773088fac4f94832729f7604746d2c0
Giacomo Caldironi <giacomo.caldironi@dgroove.it>
2024-06-27 11:27:24 +0200

    edit

--- ExcelGenerator.Test/Program.cs
+++ ExcelGenerator.Test/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<TestModel> model = new()
+            {
+                new TestModel 
+                {
+
+                }
+            };
+        }
+
+        private class TestModel
+        {
+            public string HeaderString1 { get; set; }
+            public int HeaderInteger2 { get; set; }
+            public double HeaderDouble3 { get; set; }
+            public bool HeaderBoolean4 { get; set; }
+            public DateTime HeaderDateTime5 { get; set; }
         }
     }
 }
